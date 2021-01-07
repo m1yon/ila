5c064b861bbf31957e3b99bad49df1add1088ebe
Isaiah Fielder <isaiahmfielder@gmail.com>
2021-01-06 18:17:10 -0700

    use npx w rimraf

--- src/utils/createIndex.js
+++ src/utils/createIndex.js
@@ -35,8 +35,8 @@ export const handler = async ({ a, b }) => {
   await fs.writeFile(`src/index.${ts ? 'ts' : 'js'}`, index);
 
   // remove boilerplate junk
-  await exec('rimraf ./src/handlers');
-  await exec('rimraf ./__tests__/*');
+  await exec('npx rimraf ./src/handlers');
+  await exec('npx rimraf ./__tests__/*');
 
   spinner.end();
   reporter.success(`index.${ts ? 'ts' : 'js'} created`);
